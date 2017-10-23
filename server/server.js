f7de7776e75256f9c1d3568188f5d2363aad1321
Travis Surmi <surmi.travis@gmail.com>
2017-10-22 22:04:56 -0700

    worked on backend/server

--- server/server.js
+++ server/server.js
@@ -2,19 +2,19 @@ const express = require('express')
 const session = require('express-session')
 const passport = require('passport')
 const bodyParser = require('body-parser')
-
+const userRoutes = require('./routes/user')
 const mongoose = require('mongoose')
 
 const app = express()
 const port = 3001
 app.set('trust proxy', '127.0.0.1')
 
-mongoose.connect('mongodb://localhost/__DB_PROJECT__')
+mongoose.connect('mongodb://localhost/NPR_GENERATOR')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({
-  name: '__PROJECT__',
+  name: 'NPR_GENERATOR',
   secret: '111',
   secure: false
 }))
@@ -25,6 +25,8 @@ require('./passport/strategies')(passport) // pass passport for configuration
 require('./passport/routes')(app, passport) // load our routes and pass in our app and fully configured passport
 
 app.use(require('./config/error-handler'))
+
+app.user('/api/users', userRoutes)
 
 const server = app.listen(port, () => console.log(`Running on port: ${port}`))
 
